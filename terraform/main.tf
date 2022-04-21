c6ec48efa51eaaf5a035630546018e7c91ca4890
Anand-984 <90951714+Anand-984@users.noreply.github.com>
2022-04-21 14:32:52 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 ###manju@manju-Aspire-ES1-531:~/provider$ cat main.tf      
 module "us" {
-    source ="./modules/vpc_module"       #in the current directory create create "modules/vpc_modules"   directory 
-    providers = {
+    source ="./modules/vpc_module"       #in the current directory create create "modules/vpc_modules"directory to create vpc.tf and var.tf 
+    providers = {                        # or else create vpc.tf and var.tf in the current directory and then mention source as source ="."
      aws = aws.us
     }
     vpc_cidr = "10.0.0.0/16"
